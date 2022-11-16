0bbe128e6b3a5f3d4c5fa555948ca9521334f2d8
Asep Rojali <asep.rojali@pajakku.com>
2022-11-16 13:23:28 +0700

    fix passay

--- src/main/java/io/github/avew/validator/PasswordConstraintValidator.java
+++ src/main/java/io/github/avew/validator/PasswordConstraintValidator.java
@@ -1,16 +1,27 @@
 package io.github.avew.validator;
 
+import lombok.SneakyThrows;
 import org.passay.*;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 
 public class PasswordConstraintValidator implements ConstraintValidator<PasswordConstraintValidation, String> {
 
+    @SneakyThrows
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
-        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+
+        Properties props = new Properties();
+        InputStream inputStream = getClass()
+                .getClassLoader().getResourceAsStream("passay.properties");
+        props.load(inputStream);
+        MessageResolver resolver = new PropertiesMessageResolver(props);
+        PasswordValidator validator = new PasswordValidator(resolver, Arrays.asList(
                 new LengthRule(8, 64),
                 new CharacterRule(EnglishCharacterData.LowerCase, 1),
                 new CharacterRule(EnglishCharacterData.UpperCase, 1),
@@ -25,12 +36,19 @@ public class PasswordConstraintValidator implements ConstraintValidator<Password
         if (result.isValid()) {
             return true;
         }
-        context.disableDefaultConstraintViolation();
-//		context.buildConstraintViolationWithTemplate(
-//						Joiner.on(",").join(validator.getMessages(result)))
-//				.addConstraintViolation();
-        validator.getMessages(result)
-                .forEach(message -> context.buildConstraintViolationWithTemplate(message).addConstraintViolation());
+//        context.disableDefaultConstraintViolation();
+////		context.buildConstraintViolationWithTemplate(
+////						Joiner.on(",").join(validator.getMessages(result)))
+////				.addConstraintViolation();
+//        validator.getMessages(result)
+//                .forEach(message -> context.buildConstraintViolationWithTemplate(message).addConstraintViolation());
+//        return false;
+
+        List<String> messages = validator.getMessages(result);
+        String messageTemplate = String.join(",", messages);
+        context.buildConstraintViolationWithTemplate(messageTemplate)
+                .addConstraintViolation()
+                .disableDefaultConstraintViolation();
         return false;
     }
 }
