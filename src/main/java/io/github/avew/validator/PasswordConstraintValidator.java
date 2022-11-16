ba1d6b6637f48379f4dd24d00fe61c4dd92d524f
Asep Rojali <asep.rojali@pajakku.com>
2022-11-16 13:52:06 +0700

    fix passay

--- src/main/java/io/github/avew/validator/PasswordConstraintValidator.java
+++ src/main/java/io/github/avew/validator/PasswordConstraintValidator.java
@@ -16,39 +16,29 @@ public class PasswordConstraintValidator implements ConstraintValidator<Password
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
 
-        Properties props = new Properties();
-        InputStream inputStream = getClass()
-                .getClassLoader().getResourceAsStream("passay.properties");
-        props.load(inputStream);
-        MessageResolver resolver = new PropertiesMessageResolver(props);
-        PasswordValidator validator = new PasswordValidator(resolver, Arrays.asList(
+
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
                 new LengthRule(8, 64),
-                new CharacterRule(EnglishCharacterData.LowerCase, 1),
-                new CharacterRule(EnglishCharacterData.UpperCase, 1),
-                new CharacterRule(EnglishCharacterData.Digit, 1),
-                new CharacterRule(EnglishCharacterData.Special, 1),
-                new IllegalSequenceRule(EnglishSequenceData.Alphabetical, 3, false),
-                new IllegalSequenceRule(EnglishSequenceData.Numerical, 3, false),
-                new IllegalSequenceRule(EnglishSequenceData.USQwerty, 3, false),
+                new LowercaseCharacterRule(1),
+                new UppercaseCharacterRule(1),
+                new DigitCharacterRule(1),
+                new SpecialCharacterRule(1),
+                new AlphabeticalCharacterRule(3),
+                new NumericalSequenceRule(3, false),
+                new QwertySequenceRule(3, false),
                 new WhitespaceRule()));
 
         RuleResult result = validator.validate(new PasswordData(password));
         if (result.isValid()) {
             return true;
         }
-//        context.disableDefaultConstraintViolation();
+        context.disableDefaultConstraintViolation();
 ////		context.buildConstraintViolationWithTemplate(
 ////						Joiner.on(",").join(validator.getMessages(result)))
 ////				.addConstraintViolation();
-//        validator.getMessages(result)
-//                .forEach(message -> context.buildConstraintViolationWithTemplate(message).addConstraintViolation());
-//        return false;
-
-        List<String> messages = validator.getMessages(result);
-        String messageTemplate = String.join(",", messages);
-        context.buildConstraintViolationWithTemplate(messageTemplate)
-                .addConstraintViolation()
-                .disableDefaultConstraintViolation();
+        validator.getMessages(result)
+                .forEach(message -> context.buildConstraintViolationWithTemplate(message).addConstraintViolation());
         return false;
+
     }
 }
